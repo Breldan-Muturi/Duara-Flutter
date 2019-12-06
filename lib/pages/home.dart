668357f5f92b7567afe1f1ee4c519ee9ff2418ba
Turi-Byte <breldanmuturi001@gmail.com>
2019-12-07 01:44:03 +0300

    Complete Authentication flow

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class Home extends StatefulWidget {
   @override
@@ -7,10 +10,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  bool isAuth = true;
-  
+  bool isAuth = false;
+  @override
+  void initState() {
+    super.initState();
+    //Detects when our user signed in
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (err) {
+      print('Error signing in $err');
+    });
+    //Reauthenticate user when app is opened
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignIn(account);
+    }).catchError((err) {
+      print('Error signing in $err');
+    });
+  }
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in: $account');
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  logout(){
+    googleSignIn.signOut();
+  }
   Widget buildAuthScreen() {
-    return Text('Authenticated');
+    return RaisedButton(
+      child: Text('Logout'),
+      onPressed: logout,
+    );
   }
 
   buildUnAuthScreen() {
@@ -20,41 +62,43 @@ class _HomeState extends State<Home> {
           gradient: LinearGradient(
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
-            colors: [Colors.teal,Colors.purple] 
-          )
+            colors: [
+              Theme.of(context).primaryColor,
+              Theme.of(context).accentColor
+            ],
+          ),
         ),
         alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text('Duara',
-            style: TextStyle(
-              fontFamily: "Signatra",
-              fontSize: 90.0,
-              color: Colors.white
+            Text(
+              'Duara',
+              style: TextStyle(
+                  fontFamily: "Signatra", fontSize: 90.0, color: Colors.white),
             ),
-           ),
-           GestureDetector(
-             onTap: () => print('tapped'),
-             child: Container(
-               width: 260.0,
-               height: 60.0,
-               decoration: BoxDecoration(
-               image: DecorationImage(
-                 image: AssetImage('assets/images/google_signin_button.png'),
-                 fit: BoxFit.cover
-                 ),
-               ),
-             ),
-           ),
+            GestureDetector(
+              onTap: login,
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage('assets/images/google_signin_button.png'),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
-    return isAuth ? buildAuthScreen(): buildUnAuthScreen();
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
