bb1d24ae65c30b6908aae31525e30562e0f93a90
Turi-Byte <breldanmuturi001@gmail.com>
2019-12-07 02:48:21 +0300

    Navigation and Pages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/activity_feed.dart';
+import 'package:fluttershare/pages/profile.dart';
+import 'package:fluttershare/pages/search.dart';
+import 'package:fluttershare/pages/timeline.dart';
+import 'package:fluttershare/pages/upload.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -11,9 +16,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool isAuth = false;
+  PageController pageController;
+  int pageIndex = 0;
   @override
   void initState() {
     super.initState();
+    pageController = PageController();
     //Detects when our user signed in
     googleSignIn.onCurrentUserChanged.listen((account) {
       handleSignIn(account);
@@ -41,6 +49,12 @@ class _HomeState extends State<Home> {
     }
   }
 
+  @override
+  void dispose() { 
+    pageController.dispose();
+    super.dispose();
+  }
+
   login() {
     googleSignIn.signIn();
   }
@@ -48,14 +62,50 @@ class _HomeState extends State<Home> {
   logout(){
     googleSignIn.signOut();
   }
-  Widget buildAuthScreen() {
-    return RaisedButton(
-      child: Text('Logout'),
-      onPressed: logout,
+
+  onPageChanged(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onTap(int pageIndex) {
+    pageController.jumpToPage(
+      pageIndex,
+      );
+  }
+
+  Scaffold buildAuthScreen() {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          Timeline(),
+          ActivityFeed(),
+          Upload(),
+          Search(),
+          Profile(),
+        ],
+      controller: pageController,
+      onPageChanged: onPageChanged,
+      physics: NeverScrollableScrollPhysics(), //This means the user cannot scroll
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: pageIndex,
+        onTap: onTap,
+        activeColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.whatshot),),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_active),),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_camera, size: 
+          35.0,),),
+          BottomNavigationBarItem(icon: Icon(Icons.search),),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle),),
+        ],
+      ),
     );
   }
 
-  buildUnAuthScreen() {
+  buildUnAuthScreen() { 
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
